076ba00ba68412669fc3357d7463ab947c876ab3
vgguerra <victorevandro94@gmail.com>
2024-09-15 19:48:39 -0300

    Authentication stateless with JWT Tokens and Roles

--- src/main/java/com/blogpessoal/controllers/PostController.java
+++ src/main/java/com/blogpessoal/controllers/PostController.java
@@ -2,11 +2,14 @@ package com.blogpessoal.controllers;
 
 import com.blogpessoal.dtos.PostDTO;
 import com.blogpessoal.models.Post;
+import com.blogpessoal.models.Users;
 import com.blogpessoal.services.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/post")
@@ -19,6 +22,12 @@ public class PostController {
     public ResponseEntity<Post> createNewPost(@RequestBody PostDTO postDTO) throws Exception {
         Post newPost = this.postService.newPost(postDTO);
         return new ResponseEntity<>(newPost, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Post>> getAllPosts() throws Exception {
+        List<Post> posts = this.postService.getAllPosts();
+        return new ResponseEntity<>(posts,HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
